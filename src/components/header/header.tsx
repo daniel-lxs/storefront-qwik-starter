9b270c3e0ff1163b248f742818fa0aabdbbf8c10
Daniel Riccio <ricciodaniel98@gmail.com>
2024-05-21 16:56:15 -0500

    chore: Update header component layout and styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -45,20 +45,22 @@ export default component$(() => {
 	});
 
 	return (
-		<div class={`bg-white transform shadow-xl sticky top-0 z-10 animate-dropIn`}>
+		<div class="bg-white transform shadow-xl sticky top-0 z-10 animate-dropIn">
 			<header>
-				<div class="max-w-6xl mx-auto p-4 flex items-center space-x-4">
-					<button
-						class="block sm:hidden text-primary"
-						onClick$={() => (appState.showMenu = !appState.showMenu)}
-					>
-						<MenuIcon />
-					</button>
-					<h1 class="text-primary w-10">
-						<Link href="/">
-							<img src={`/logo-72-72.png`} width={40} height={31} alt="Vendure logo" />
-						</Link>
-					</h1>
+				<div class="max-w-6xl mx-auto p-4 flex items-center justify-center space-x-12 sm:space-x-8 ">
+					<div class="flex justify-center space-x-6">
+						<button
+							class="block sm:hidden text-primary"
+							onClick$={() => (appState.showMenu = !appState.showMenu)}
+						>
+							<MenuIcon />
+						</button>
+						<h1 class="text-primary w-10">
+							<Link href="/">
+								<img src={`/logo-72-72.png`} width={40} height={31} alt="Vendure logo" />
+							</Link>
+						</h1>
+					</div>
 					<div class="hidden space-x-4 sm:block">
 						<Link
 							href="/"
@@ -80,48 +82,52 @@ export default component$(() => {
 							</Link>
 						))}
 					</div>
-					<div class="flex-1 block md:pr-8">
-						<SearchBar />
+					<div>
+						<div class="hidden sm:block">
+							<SearchBar />
+						</div>
 					</div>
-					<div class="">
-						<button
-							name="Cart"
-							aria-label={`${totalQuantity} items in cart`}
-							class="relative w-9 h-9 hover:bg-primary/20 transition-colors rounded text-primary p-1"
-							onClick$={() => (appState.showCart = !appState.showCart)}
-						>
-							<LuShoppingCart class="w-6 h-6" />
-							{totalQuantity ? (
-								<div class="absolute rounded-full -top-2 -right-2 bg-primary-600 w-6 h-6">
-									{totalQuantity}
-								</div>
-							) : (
-								''
-							)}
-						</button>
-					</div>
-					<div class="flex mr-[60px] 2xl:mr-0 h-9">
-						<Link
-							href={appState.customer.id !== CUSTOMER_NOT_DEFINED_ID ? '/account' : '/sign-in'}
-							class={
-								'flex items-center space-x-1 py-4 px-2 rounded text-primary hover:bg-primary/20 transition-colors'
-							}
-						>
-							<LuUser class="w-6 h-6" />
-							<span class="mt-1 text-primary">
-								{appState.customer.id !== CUSTOMER_NOT_DEFINED_ID
-									? $localize`My Account`
-									: $localize`Sign In`}
-							</span>
-						</Link>
-						{appState.customer.id !== CUSTOMER_NOT_DEFINED_ID && (
-							<button onClick$={logout} class="text-gray-700">
-								<div class="flex items-center cursor-pointer">
-									<span class="mr-2">{$localize`Logout`}</span>
-									<LogoutIcon />
-								</div>
+					<div class="flex justify-center space-x-2">
+						<div class="">
+							<button
+								name="Cart"
+								aria-label={`${totalQuantity} items in cart`}
+								class="relative w-9 h-9 hover:bg-primary/20 transition-colors rounded text-primary p-1"
+								onClick$={() => (appState.showCart = !appState.showCart)}
+							>
+								<LuShoppingCart class="w-6 h-6" />
+								{totalQuantity ? (
+									<div class="absolute rounded-full -top-2 -right-2 bg-primary-600 w-6 h-6">
+										{totalQuantity}
+									</div>
+								) : (
+									''
+								)}
 							</button>
-						)}
+						</div>
+						<div class="flex mr-[60px] 2xl:mr-0 h-9">
+							<Link
+								href={appState.customer.id !== CUSTOMER_NOT_DEFINED_ID ? '/account' : '/sign-in'}
+								class={
+									'flex items-center space-x-1 py-4 px-2 rounded text-primary hover:bg-primary/20 transition-colors'
+								}
+							>
+								<LuUser class="w-6 h-6" />
+								<span class="mt-1 text-primary">
+									{appState.customer.id !== CUSTOMER_NOT_DEFINED_ID
+										? $localize`My Account`
+										: $localize`Sign In`}
+								</span>
+							</Link>
+							{appState.customer.id !== CUSTOMER_NOT_DEFINED_ID && (
+								<button onClick$={logout} class="text-gray-700">
+									<div class="flex items-center cursor-pointer">
+										<span class="mr-2">{$localize`Logout`}</span>
+										<LogoutIcon />
+									</div>
+								</button>
+							)}
+						</div>
 					</div>
 				</div>
 			</header>
--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,16 +1,22 @@
 import { component$ } from '@builder.io/qwik';
+import { LuSearch } from '@qwikest/icons/lucide';
 
 export default component$(() => {
 	return (
 		<form action="/search">
-			<input
-				type="search"
-				name="q"
-				default-value={''}
-				placeholder={$localize`Search`}
-				autoComplete="off"
-				class="shadow-sm focus:ring-primary-500 focus:border-primary-500 block w-full sm:text-sm border-gray-300 rounded-md"
-			/>
+			<div class="flex items-center">
+				<div class="relative">
+					<input
+						type="search"
+						name="q"
+						defaultValue={''}
+						placeholder={$localize`Search`}
+						autoComplete="off"
+						class="shadow-sm focus:ring-primary focus:border-primary block w-full sm:text-sm border-gray-300 rounded-xl pl-10 md:max-w-96"
+					/>
+					<LuSearch class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+				</div>
+			</div>
 		</form>
 	);
 });

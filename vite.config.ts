4b03ba39051306940b611ff62702c1b25361cf55
Daniel Riccio <ricciodaniel98@gmail.com>
2024-05-21 14:33:03 -0500

    chore: Update package dependencies and configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,23 @@ import { qwikVite } from '@builder.io/qwik/optimizer';
 import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(async (config) => {
-	return {
-		// Enable to analyze via source-map-explorer
-		ssr: { target: 'webworker' },
-		build: {
-			sourcemap: config.mode === 'development',
+export default defineConfig({
+	// Enable to analyze via source-map-explorer
+	ssr: { target: 'webworker' },
+	build: {
+		sourcemap: process.env.NODE_ENV === 'development',
+	},
+	plugins: [
+		qwikInsights({
+			publicApiKey: loadEnv('', '.', '').VITE_QWIK_INSIGHTS_KEY,
+		}),
+		qwikCity(),
+		qwikVite(),
+		tsconfigPaths(),
+	],
+	preview: {
+		headers: {
+			'Cache-Control': 'public, max-age=600',
 		},
-		plugins: [
-			qwikInsights({
-				publicApiKey: loadEnv('', '.', '').VITE_QWIK_INSIGHTS_KEY,
-			}),
-			qwikCity(),
-			qwikVite(),
-			tsconfigPaths(),
-		],
-		preview: {
-			headers: {
-				'Cache-Control': 'public, max-age=600',
-			},
-		},
-	};
+	},
 });

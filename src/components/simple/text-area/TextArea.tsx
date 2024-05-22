507ce55922c4439e1dcd5f31cef6bcba91c35919
Daniel Riccio <ricciodaniel98@gmail.com>
2024-05-22 17:17:12 -0500

    feat: Create TextArea component for displaying a text area input field

---
+++ src/components/simple/text-area/TextArea.tsx
@@ -0,0 +1,32 @@
+import { $, PropFunction, component$ } from '@builder.io/qwik';
+
+interface InputProps {
+	label: string;
+	value: string;
+	onChange$?: PropFunction<(event: Event, el: HTMLTextAreaElement) => void>;
+	onKeyUp$?: PropFunction<(event: KeyboardEvent, el: HTMLTextAreaElement) => void>;
+	onBlur$?: PropFunction<(event: FocusEvent, el: HTMLTextAreaElement) => void>;
+}
+
+export default component$((props: InputProps) => {
+	const handleChange = $((event: Event, el: HTMLTextAreaElement) => {
+		props.onChange$?.(event, el);
+	});
+
+	return (
+		<div>
+			<label class="block text-sm font-medium text-gray-700">{props.label}</label>
+			<div class="mt-1">
+				<textarea
+					value={props.value}
+					required
+					rows={10}
+					onInput$={handleChange}
+					onKeyUp$={props.onKeyUp$}
+					onBlur$={props.onBlur$}
+					class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
+				/>
+			</div>
+		</div>
+	);
+});

839056f13a6f8c533926fee4d343c1592ea6d117
Daniel Riccio <ricciodaniel98@gmail.com>
2024-05-21 19:43:08 -0500

    refactor: Improve CardGallery component layout and performance

--- src/components/card-gallery/CardGallery.tsx
+++ src/components/card-gallery/CardGallery.tsx
@@ -3,13 +3,18 @@ import { Link } from '@builder.io/qwik-city';
 import { LuChevronLeft, LuChevronRight } from '@qwikest/icons/lucide';
 import { Image } from 'qwik-image';
 
+export interface CardGalleryProps {
+	title: string;
+	items: Item[];
+}
+
 export type Item = {
 	title: string;
 	imageUrl: string;
 	link: string;
 };
 
-export default component$<{ items: Item[] }>(({ items }) => {
+export default component$(({ title, items }: CardGalleryProps) => {
 	const scrollContainer = useSignal<HTMLDivElement>();
 	const showLeftButton = useSignal<boolean>(false);
 	const showRightButton = useSignal<boolean>(true);
@@ -47,44 +52,49 @@ export default component$<{ items: Item[] }>(({ items }) => {
 	});
 
 	return (
-		<div class="relative">
-			{showLeftButton.value && (
-				<button
-					onClick$={scrollLeft}
-					class="absolute left-4 sm:left-6 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-4 rounded-full z-10"
-				>
-					<LuChevronLeft class="w-4 sm:w-6 h-4 sm:h-6" />
-				</button>
-			)}
-			<div ref={scrollContainer} class="flex overflow-x-auto space-x-4 p-4">
-				{items.map((item, index) => (
-					<Link href={item.link} key={index} class="flex-shrink-0 max-w-[300px]">
-						<div class="relative rounded-lg overflow-hidden hover:opacity-75 mx-auto">
-							<div class="w-full h-full object-center object-cover">
-								<Image
-									layout="fixed"
-									width="300"
-									height="300"
-									src={item.imageUrl}
-									alt={item.title}
-								/>
-							</div>
-							<span class="absolute w-full bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50" />
-							<span class="absolute w-full bottom-2 mt-auto text-center text-xl font-bold text-white">
-								{item.title}
-							</span>
-						</div>
-					</Link>
-				))}
+		<div class="relative space-y-2 pt-6">
+			<div class="sm:px-4 lg:px-6">
+				<h2 class="text-2xl font-light tracking-tight text-gray-900">{title}</h2>
 			</div>
-			{showRightButton.value && (
-				<button
-					onClick$={scrollRight}
-					class="absolute right-4 sm:right-6 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-4 rounded-full z-10"
-				>
-					<LuChevronRight class="w-4 sm:w-6 h-4 sm:h-6" />
-				</button>
-			)}
+			<div>
+				{showLeftButton.value && (
+					<button
+						onClick$={scrollLeft}
+						class="absolute left-4 sm:left-6 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-4 rounded-full z-10"
+					>
+						<LuChevronLeft class="w-4 sm:w-6 h-4 sm:h-6" />
+					</button>
+				)}
+				<div ref={scrollContainer} class="flex overflow-x-auto space-x-4 p-4">
+					{items.map((item, index) => (
+						<Link href={item.link} key={index} class="flex-shrink-0 max-w-[300px]">
+							<div class="relative rounded-lg overflow-hidden hover:opacity-75 mx-auto">
+								<div class="w-full h-full object-center object-cover">
+									<Image
+										layout="fixed"
+										width="300"
+										height="300"
+										src={item.imageUrl}
+										alt={item.title}
+									/>
+								</div>
+								<span class="absolute w-full bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50" />
+								<span class="absolute w-full bottom-2 mt-auto text-center text-xl font-bold text-white">
+									{item.title}
+								</span>
+							</div>
+						</Link>
+					))}
+				</div>
+				{showRightButton.value && (
+					<button
+						onClick$={scrollRight}
+						class="absolute right-4 sm:right-6 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-4 rounded-full z-10"
+					>
+						<LuChevronRight class="w-4 sm:w-6 h-4 sm:h-6" />
+					</button>
+				)}
+			</div>
 		</div>
 	);
 });

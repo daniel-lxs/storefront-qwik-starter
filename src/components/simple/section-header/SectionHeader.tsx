05dcd5a3f2e0210895e2f62162753a1e52e93d03
Daniel Riccio <ricciodaniel98@gmail.com>
2024-05-22 23:21:37 -0500

    refactor: Update SectionHeader component styling and layout

--- src/components/simple/section-header/SectionHeader.tsx
+++ src/components/simple/section-header/SectionHeader.tsx
@@ -19,7 +19,7 @@ export default component$<Props>(({ title, subtitle, extraClass }) => {
 					width="800"
 					class="object-contain object-center w-full h-full"
 				/>
-				<div class="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
+				<div class="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center px-4 lg:px-8">
 					<h2 class="text-2xl sm:text-4xl text-white font-bold">{title}</h2>
 					<p class="mt-2 text-lg sm:text-xl text-white text-center">{subtitle}</p>
 				</div>
